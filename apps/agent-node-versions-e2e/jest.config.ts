b54906b3770785e05331bb676bfe7bc7c3b5b8d2
Philip Fulcher <philip@nrwl.io>
2024-04-16 15:07:41 -0600

    Initial commit

---
+++ apps/agent-node-versions-e2e/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'agent-node-versions-e2e',
+  preset: '../..//jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../..//coverage/agent-node-versions-e2e',
+};

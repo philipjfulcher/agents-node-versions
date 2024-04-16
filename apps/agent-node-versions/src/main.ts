669a9d4cb3edaaec996bfa2e4a68e687a0270468
Philip Fulcher <philip@nrwl.io>
2024-04-16 15:18:39 -0600

    adding agents

--- apps/agent-node-versions/src/main.ts
+++ apps/agent-node-versions/src/main.ts
@@ -1,1 +1,18 @@
 console.log('Hello World');
+
+const strings = [
+  // Lone leading surrogate
+  'ab\uD800',
+  'ab\uD800c',
+  // Lone trailing surrogate
+  '\uDFFFab',
+  'c\uDFFFab',
+  // Well-formed
+  'abc',
+  'ab\uD83D\uDE04c',
+];
+
+for (const str of strings) {
+  // @ts-expect-error isWellFormed is node 20 feature
+  console.log(str.isWellFormed());
+}
